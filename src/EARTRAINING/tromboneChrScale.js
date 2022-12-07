05005edefe0ac43fed8a22e639605d5229ac6df9
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-06 22:23:35 -0800

    added trombone chr scale audio files

---
+++ src/EARTRAINING/tromboneChrScale.js
@@ -0,0 +1,147 @@
+import low_Eb from "./trombone_chr_scale/bone_low_Eb.mp3";
+import low_E from "./trombone_chr_scale/bone_low_E.mp3";
+import low_F from "./trombone_chr_scale/bone_low_F.mp3";
+import low_Gb from "./trombone_chr_scale/bone_low_Gb.mp3";
+import low_G from "./trombone_chr_scale/bone_low_G.mp3";
+import low_Ab from "./trombone_chr_scale/bone_low_Ab.mp3";
+import low_A from "./trombone_chr_scale/bone_low_A.mp3";
+import low_Bb from "./trombone_chr_scale/bone_low_Bb.mp3";
+import low_B from "./trombone_chr_scale/bone_low_B.mp3";
+import low_C from "./trombone_chr_scale/bone_low_C.mp3";
+import low_Db from "./trombone_chr_scale/bone_low_Db.mp3";
+import low_D from "./trombone_chr_scale/bone_low_D.mp3";
+import Eb from "./trombone_chr_scale/bone_Eb.mp3";
+import E from "./trombone_chr_scale/bone_E.mp3";
+import F from "./trombone_chr_scale/bone_F.mp3";
+import Gb from "./trombone_chr_scale/bone_Gb.mp3";
+import G from "./trombone_chr_scale/bone_G.mp3";
+import Ab from "./trombone_chr_scale/bone_Ab.mp3";
+import A from "./trombone_chr_scale/bone_A.mp3";
+import Bb from "./trombone_chr_scale/bone_Bb.mp3";
+import B from "./trombone_chr_scale/bone_B.mp3";
+import C from "./trombone_chr_scale/bone_C.mp3";
+import Db from "./trombone_chr_scale/bone_Db.mp3";
+import D from "./trombone_chr_scale/bone_high_D.mp3";
+
+export const tromboneChrScale = [
+  {
+    index: 0,
+    name: low_Eb,
+    audio_file: new Audio(low_Eb),
+  },
+  {
+    index: 1,
+    name: low_E,
+    audio_file: new Audio(low_E),
+  },
+  {
+    index: 2,
+    name: low_F,
+    audio_file: new Audio(low_F),
+  },
+  {
+    index: 3,
+    name: low_Gb,
+    audio_file: new Audio(low_Gb),
+  },
+  {
+    index: 4,
+    name: low_G,
+    audio_file: new Audio(low_G),
+  },
+  {
+    index: 5,
+    name: low_Ab,
+    audio_file: new Audio(low_Ab),
+  },
+  {
+    index: 6,
+    name: low_A,
+    audio_file: new Audio(low_A),
+  },
+  {
+    index: 7,
+    name: low_Bb,
+    audio_file: new Audio(low_Bb),
+  },
+  {
+    index: 8,
+    name: low_B,
+    audio_file: new Audio(low_B),
+  },
+  {
+    index: 9,
+    name: low_C,
+    audio_file: new Audio(low_C),
+  },
+  {
+    index: 10,
+    name: low_Db,
+    audio_file: new Audio(low_Db),
+  },
+  {
+    index: 11,
+    name: low_D,
+    audio_file: new Audio(low_D),
+  },
+  {
+    index: 12,
+    name: Eb,
+    audio_file: new Audio(Eb),
+  },
+  {
+    index: 13,
+    name: E,
+    audio_file: new Audio(E),
+  },
+  {
+    index: 14,
+    name: F,
+    audio_file: new Audio(F),
+  },
+  {
+    index: 15,
+    name: Gb,
+    audio_file: new Audio(Gb),
+  },
+  {
+    index: 16,
+    name: G,
+    audio_file: new Audio(G),
+  },
+  {
+    index: 17,
+    name: Ab,
+    audio_file: new Audio(Ab),
+  },
+  {
+    index: 18,
+    name: A,
+    audio_file: new Audio(A),
+  },
+  {
+    index: 19,
+    name: Bb,
+    audio_file: new Audio(B),
+  },
+  {
+    index: 20,
+    name: B,
+    audio_file: new Audio(B),
+  },
+  {
+    index: 21,
+    name: C,
+    audio_file: new Audio(C),
+  },
+  {
+    index: 22,
+    name: Db,
+    audio_file: new Audio(Db),
+  },
+  {
+    index: 23,
+    name: D,
+    audio_file: new Audio(D),
+  },
+];
