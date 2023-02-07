ffb341224315c49d3d55d649a5b36eb8fa8c3cc3
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-07 15:25:33 -0800

    deleted a few more unused files

--- src/features/RandomQuestionSlice.js
+++
@@ -1,7 +0,0 @@
-export const RandomQuestionSlice = (questionsArray) => {
-  const randomQuestion =
-    questionsArray[Math.floor(Math.random() * questionsArray.length)];
-  return randomQuestion;
-};
-
-export default RandomQuestionSlice;
